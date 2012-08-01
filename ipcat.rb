cf799002832c1154d5aa99e94df79cd9dad41fa5
Aaron Suggs <aaron@ktheory.com>
2012-08-01 11:40:59 -0400

    wip

--- ipcat.rb
+++ ipcat.rb
@@ -4,12 +4,13 @@
 
 require 'ipaddr'
 require 'open-uri'
-require 'bsearch'
 
 class IPCat
+  VERSION = '0.0.1'
+
   class << self
     def matches?(ip)
-      BSearch.bsearch(ip_to_fixnum(ip), ranges)
+      bsearch(ip_to_fixnum(ip), ranges)
     end
 
     def ip_to_fixnum(ip)
@@ -26,6 +27,34 @@ class IPCat
           first, last = line.split(',')[0,2]
         IPRange.new(first, last)
       end
+    end
+
+    # Assume's haystack is an array of comparable objects
+    def bsearch(needle, haystack, first=0, last=nil)
+      last ||= haystack.size - 1
+      return nil if last < first # not found, or empty haystack
+
+      cur = first + (last - first)/2
+      case haystack[cur] <=> needle
+      when -1 # needle is larger than cur value
+        bsearch(needle, haystack, cur+1, last)
+      when 1 # needle is smaller than cur value
+        bsearch(needle, haystack, first, cur-1)
+      when 0
+        cur
+      end
+    end
+
+    # Sigh, why isn't this part of IPAddr?
+    def i_to_ip_str(i)
+      exp = 1
+      parts = []
+      while i > 0
+        parts << i % 256
+        i /= 256
+        exp += 1
+      end
+      parts.reverse * '.'
     end
   end
 
@@ -54,6 +83,10 @@ class IPCat
         # Assume all IPRanges are non-overlapping
         first <=> obj.first
       end
+    end
+
+    def size
+      last - first
     end
 
     protected
