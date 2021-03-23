f504defd76fdfbd013c3382f56fb6d66b29afe93
Alexander Mancevice <alexander.mancevice@hey.com>
2021-03-23 11:36:21 -0400

    Update spec
    Replace deprecated syntax with newer Minitest 5+

--- spec/ipcat_iprange_spec.rb
+++ spec/ipcat_iprange_spec.rb
@@ -14,14 +14,14 @@ describe 'IPCat::IPRange' do
 
   describe '#initialize' do
     it 'should fail if last < first' do
-      -> { IPCat::IPRange.new(2, 1) }.must_raise ArgumentError
+      expect { IPCat::IPRange.new(2, 1) }.must_raise ArgumentError
     end
   end
 
   describe '#<=> for integers' do
-    it('should match first')   { (range <=> range.first).must_equal 0 }
-    it('should match last')    { (range <=> range.last).must_equal 0 }
-    it('should match first-1') { (range <=> range.first - 1).must_equal 1 }
-    it('should match last+1')  { (range <=> range.last + 1).must_equal(-1) }
+    it('should match first')   { expect(range <=> range.first).must_equal 0 }
+    it('should match last')    { expect(range <=> range.last).must_equal 0 }
+    it('should match first-1') { expect(range <=> range.first - 1).must_equal 1 }
+    it('should match last+1')  { expect(range <=> range.last + 1).must_equal(-1) }
   end
 end
